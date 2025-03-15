ba488f156b8b4ff61a4ff0fc244b6103ec74c659
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-03-15 14:12:56 +0600

    final

--- src/pages/AllJob/AllJob.jsx
+++ src/pages/AllJob/AllJob.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import useJobs from "../../hooks/useJobs";
 import HotJobCard from "../Home/HotJobCard";
+import { BiSearch } from "react-icons/bi";
 
 const AllJob = () => {
   const [sort, setSort] = useState(false);
-  const { jobs, loading } = useJobs(sort);
+  const [minSalary, setMinSalary] = useState("");
+  const [maxSalary, setMaxSalary] = useState("");
+  const [search, setSearch] = useState("");
+  const { jobs, loading } = useJobs(sort, search, minSalary, maxSalary);
 
   if (loading) {
     return <h2>Job Is Loading...</h2>;
@@ -14,13 +18,35 @@ const AllJob = () => {
     <div>
       <h1 className="py-5 text-4xl font-bold text-center">All Jobs</h1>
 
-      <div className="w-11/12 mx-auto bg-base-200 py-5 p-3 flex items-center">
+      <div className="w-11/12 mx-auto bg-base-200 py-5 p-3 flex items-center gap-5 rounded-md mb-5">
         <button
           onClick={() => setSort(!sort)}
           className={`btn btn-neutral ${sort && "btn-success"}`}
         >
           {sort ? "Sorted By Salary" : "Sort By Salary"}
         </button>
+        <BiSearch></BiSearch>
+        <input
+          onKeyUp={(e) => setSearch(e.target.value)}
+          type="text"
+          className="input w-full max-w-2xl"
+          placeholder="Search Jobs by Location"
+        />
+
+        <div className="space-y-2">
+          <input
+            onKeyUp={(e) => setMinSalary(e.target.value)}
+            type="text"
+            className="input w-full max-w-xs"
+            placeholder="Min salary"
+          />
+          <input
+            onKeyUp={(e) => setMaxSalary(e.target.value)}
+            type="text"
+            className="input w-full max-w-xs"
+            placeholder="Max salary"
+          />
+        </div>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
